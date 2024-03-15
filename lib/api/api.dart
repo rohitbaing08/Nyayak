8d115381b0ab31a0129d932512d6e4d1859f2423
Keyur <keyurchaudhari536@gmail.com>
2024-03-15 07:38:02 +0530

    api

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -6,30 +6,45 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-Future<void> addUserToFirestore(
-  String name,
-  String email,
-  String contactNumber,
-  String gender,
-  DateTime dob,
-) async {
-  // Get a Firestore instance
-  final firestore = FirebaseFirestore.instance;
+class APIs {
+  // to return current user
+  static User get user => auth.currentUser!;
+// for accessing cloud firestore database
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-  // Prepare a collection reference (replace 'users' with your actual collection name)
-  final collectionRef = firestore.collection('users');
+  // for accessing firebase storage
+  static FirebaseStorage storage = FirebaseStorage.instance;
 
-  // Create a document with specific fields
-  final userData = {
-    'name': name,
-    'email': email,
-    'contactNumber': contactNumber,
-    'gender': gender,
-    'dob': dob, // Assuming dob is a DateTime object
-  };
+  Future<void> addUserToFirestore(
+    String name,
+    String email,
+    String contactNumber,
+    String gender,
+    DateTime dob,
+  ) async {
+    // Get a Firestore instance
+    final firestore = FirebaseFirestore.instance;
 
-  // Add the document with the user data
-  await collectionRef.add(userData);
+    // Prepare a collection reference (replace 'users' with your actual collection name)
+    final collectionRef = firestore.collection('users');
 
-  print('Successfully added user to Firestore');
+    // Create a document with specific fields
+    final userData = {
+      'name': name,
+      'email': email,
+      'contactNumber': contactNumber,
+      'gender': gender,
+      'dob': dob, // Assuming dob is a DateTime object
+    };
+
+    // Add the document with the user data
+    await collectionRef.add(userData);
+
+    print('Successfully added user to Firestore');
+  }
+
+// for checking if user exists or not?
+  Future<bool> userExists() async {
+    return (await firestore.collection('users').doc(user.uid).get()).exists;
+  }
 }
